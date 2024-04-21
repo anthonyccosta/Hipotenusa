a7c27891bf1a79b0e088cab183e261fffec4607a
anthonyccosta <ccosta.anthony@gmail.com>
2024-04-21 15:06:43 -0300

    primeiro commit

---
+++ Hipotenusa/Program.cs
@@ -0,0 +1,13 @@
+﻿    double catetoA, catetoB, hipotenusa;
+
+    Console.Write("Digite o valor do cateto A: ");
+    catetoA = double.Parse(Console.ReadLine());
+
+    Console.Write("Digite o valor do cateto B: ");
+    catetoB = double.Parse(Console.ReadLine());
+
+    // calculando a hipotenusa 
+    hipotenusa = (catetoA * catetoA + catetoB * catetoB);
+
+    // exibindo o valor da hipotenusa
+    Console.WriteLine("A hipotenusa do triângulo é: {0}", hipotenusa);
